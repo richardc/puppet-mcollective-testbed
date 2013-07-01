1871f2cf3cae57a7bc1a401a37863f3144a63ea4
Richard Clamp <richardc@unixbeard.net>
2013-07-01 09:53:21 +0100

    upgrade hiera so we don't keep getting the debug

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,6 @@
-class { 'puppetlabs_repos': } -> Class['mcollective']
+class { 'puppetlabs_repos': } ->
+package { 'hiera': ensure => latest } ->
+Class['mcollective']
 
 # a use of import - I am a professional stunt man
 import "/vagrant/modules/mcollective/spec/fixtures/manifests/end_to_end.pp"
