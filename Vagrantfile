91841afb9d58222a5758c928195400ff06605699
Richard Clamp <richardc@unixbeard.net>
2013-07-03 01:23:46 +0100

    add ubuntu testing

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,16 @@
 # vi: set ft=ruby :
 
 Vagrant::Config.run do |config|
-  config.vm.box = "centos-64"
-  config.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-64-x64-vbox4210.box"
+  config.vm.define :centos do |config|
+    config.vm.box = "centos-64"
+    config.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-64-x64-vbox4210.box"
+  end
+
+
+  config.vm.define :ubuntu do |config|
+    config.vm.box = "ubuntu1204"
+    config.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/ubuntu-server-12042-x64-vbox4210.box"
+  end
 
   config.vm.provision :puppet do |puppet|
     puppet.module_path    = "modules"
--- modules/puppetlabs_repos/manifests/init.pp
+++ modules/puppetlabs_repos/manifests/init.pp
@@ -1,30 +1,41 @@
 class puppetlabs_repos {
-  yumrepo { 'puppetlabs-devel':
-      baseurl  => 'http://yum.puppetlabs.com/el/6/devel/$basearch',
-      descr    => 'Puppet Labs Devel El 6 - $basearch',
-      enabled  => '1',
-      gpgcheck => '0'
-  } ->
+  if $::osfamily == 'RedHat' {
+    yumrepo { 'puppetlabs-devel':
+        baseurl  => 'http://yum.puppetlabs.com/el/6/devel/$basearch',
+        descr    => 'Puppet Labs Devel El 6 - $basearch',
+        enabled  => '1',
+        gpgcheck => '0'
+    } ->
 
-  yumrepo { 'puppetlabs-deps':
-      baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/$basearch',
-      descr    => 'Puppet Labs Dependencies El 6 - $basearch',
-      enabled  => '1',
-      gpgcheck => '0'
-  } ->
+    yumrepo { 'puppetlabs-deps':
+        baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/$basearch',
+        descr    => 'Puppet Labs Dependencies El 6 - $basearch',
+        enabled  => '1',
+        gpgcheck => '0'
+    } ->
 
-  yumrepo { 'puppetlabs-products':
-      baseurl  => 'http://yum.puppetlabs.com/el/6/products/$basearch',
-      descr    => 'Puppet Labs Products El 6 - $basearch',
-      enabled  => '1',
-      gpgcheck => '0'
-  } ->
+    yumrepo { 'puppetlabs-products':
+        baseurl  => 'http://yum.puppetlabs.com/el/6/products/$basearch',
+        descr    => 'Puppet Labs Products El 6 - $basearch',
+        enabled  => '1',
+        gpgcheck => '0'
+    } ->
 
-  yumrepo { 'epel':
-      descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
-      enabled        => '1',
-      failovermethod => 'priority',
-      gpgcheck       => '0',
-      mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+    yumrepo { 'epel':
+        descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
+        enabled        => '1',
+        failovermethod => 'priority',
+        gpgcheck       => '0',
+        mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+    }
+  }
+
+  if $::osfamily == 'Debian' {
+    file { '/etc/apt/sources.list.d/puppetlabs.list':
+      content => "deb http://apt.puppetlabs.com precise main dependencies\n",
+    } ~>
+    exec { '/usr/sbin/apt-get update':
+      refreshonly => true,
+    }
   }
 }
