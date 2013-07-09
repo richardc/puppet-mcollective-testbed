a4d8c59e3a1218ce8c7c8a50baf5ed0b1edff727
Richard Clamp <richardc@unixbeard.net>
2013-07-09 11:03:07 +0100

    order the repo class more implicitly

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,10 @@
-class { 'puppetlabs_repos': } ->
-package { 'hiera': ensure => latest } ->
-Class['mcollective']
+class { 'puppetlabs_repos': } -> Package <| |>
+
+if $::osfamily == 'RedHat' {
+  package { 'hiera':
+    ensure => 'latest',
+  }
+}
 
 # a use of import - I am a professional stunt man
 import "/vagrant/modules/mcollective/spec/fixtures/manifests/end_to_end.pp"
